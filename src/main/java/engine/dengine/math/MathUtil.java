f91e889955f6b9f3b28b95873ff1eb62603dfdc4
SchnfeldDev <schnfeldanton@gmail.com>
2023-11-28 22:13:22 +0100

    Changed MathUtil.isPointInRectangle from a bounding-box algorithm to a barycentric coordinates based one

--- src/main/java/engine/dengine/math/MathUtil.java
+++ src/main/java/engine/dengine/math/MathUtil.java
@@ -14,19 +14,24 @@ import engine.dengine.shapes.Triangle;
  */
 public class MathUtil
 {
+    // Prevent instantiation of utility class
+    private MathUtil () {}
+
     /**
      * Checks wether a three-dimensional point is within the boundaries of a {@link Rectangle} instance.
+     * Uses <b>barycentric</b> coordinates to check for <b>collision</b>, where the rectangle is divided
+     * into two triangles which each get checked separately.
      * @param r the {@link Rectangle} instance
      * @param x the x coordinate of the point
      * @param y the y coordinate of the point
      * @param z the z coordinate of the point
      * @return wether the point lies within the boundaries of the {@link Rectangle} instance
      */
-    public static boolean isPointInRectangle (Rectangle r, float x, float y, float z)
+    public static boolean isPointInRectangle(Rectangle r, float x, float y, float z)
     {
         float[] rectangleVertices = r.getVertices();
 
-        // If they are on different z indexes, they cant be colliding
+        // If they are on different z indexes, they can't be colliding
         if (z != rectangleVertices[2]) return false;
 
         // Extract vertices of the rectangle
@@ -39,11 +44,36 @@ public class MathUtil
         float x4 = rectangleVertices[9];
         float y4 = rectangleVertices[10];
 
-        // Check if the point is inside the rectangle using the bounding box algorithm
-        return x >= Math.min(x1, x2) && x <= Math.max(x1, x2) && // Is x within the x bounds of the rectangle
-                y >= Math.min(y1, y3) && y <= Math.max(y1, y3);  // Is y within the y bounds of the rectangle
+        // First triangle
+        // Calculate barycentric coordinates for the first triangle
+        float detT1 = (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3);
+        float alpha1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / detT1;
+        float beta1 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / detT1;
+        float gamma1 = 1 - alpha1 - beta1;
+
+        // Check if the point is inside the first triangle
+        if (alpha1 >= 0 && beta1 >= 0 && gamma1 >= 0) return true;
+
+        // Second Triangle
+        // Calculate barycentric coordinates for the second triangle
+        float detT2 = (y4 - y1) * (x3 - x1) + (x1 - x4) * (y3 - y1);
+        float alpha2 = ((y4 - y1) * (x - x1) + (x1 - x4) * (y - y1)) / detT2;
+        float beta2 = ((y1 - y3) * (x - x1) + (x3 - x1) * (y - y1)) / detT2;
+        float gamma2 = 1 - alpha2 - beta2;
+
+        // Check if the point is inside the second triangle
+        return alpha2 >= 0 && beta2 >= 0 && gamma2 >= 0;
     }
 
+    /**
+     * Checks wether a three-dimensional point is within the boundaries of a {@link Triangle} instance.
+     * Uses <b>barycentric</b> coordinates to check for <b>collision</b>.
+     * @param t the {@link Triangle} instance
+     * @param x the x coordinate of the point
+     * @param y the y coordinate of the point
+     * @param z the z coordinate of the point
+     * @return wether the point lies within the boundaries of the {@link Triangle} instance
+     */
     public static boolean isPointInTriangle (Triangle t, float x, float y, float z)
     {
         // Implementation of a point-in-triangle test using barycentric coordinates
